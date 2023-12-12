1b76a7e5c5e0977d5247cf10560cd5696dfdcefb
b202001162 <202001162@daiict.ac.in>
2023-12-12 18:28:19 +0530

    Route, Routes are working

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { BrowserRouter, Routes, Route} from "react-router-dom";
+import Nav from "./Nav";
 
 function App() {
   return (
     <div className="App">
-      <h1>React App</h1>
+      <BrowserRouter>
+        <Nav />
+        <Routes>
+          <Route path="/" element={<h1>Home</h1>}></Route>
+          <Route path="/about" element={<h1>About</h1>}></Route>
+          <Route path="/contact" element={<h1>Contact</h1>}></Route>
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
